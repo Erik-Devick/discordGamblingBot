664f8827d645ad4b1b3f101653e89d552cfddee4
erik_devick <erikdevick1@gmail.com>
2024-12-23 14:34:09 -0600

    commit

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def help(ctx):
     body = [["User","help",">help","Displays this message"],
             ["User","register",">register","Creates account for user balance"],
             ["User","balance",">balance","Display user balance"],
-            ["User","reset",">reset","Resets balance to $1000"],
+            ["User","reset",">reset","Resets balance to $1000.00"],
             ["User","leaderboard",">leaderboard","Displays all users sorted by balance"],
             ["Game","highlow",">highlow <bet>","plays the higher or lower game"],
             ["Game","coinflip",">coinflip <bet> <side>","bet on the outcome of a coin flip"]]
@@ -46,14 +46,12 @@ async def register(ctx):
         users = {}
 
     if user in users.keys():
-        await ctx.send("You are already registered")
-        await ctx.send(f"Balance: ${users[user]}")
+        await ctx.send(f"```You are already registered.\nBalance: ${users[user]}```")
     else:
-        users[user] = 1000
+        users[user] = format(1000,".2f")
         with open("users.json", "w") as file:
             json.dump(users, file)
-        await ctx.send("You are now registered.")
-        await ctx.send("Balance: $1000")
+        await ctx.send("```You are now registered.\nBalance: $1000.00```")
 
 #check balance
 @bot.command()
@@ -63,12 +61,12 @@ async def balance(ctx):
         with open("users.json") as file:
             users = json.load(file)
     except (json.JSONDecodeError):
-        await ctx.send("You are not registered")
+        await ctx.send("```You are not registered. Please type '>register'.```")
         return
     if user in users.keys():
-        await ctx.send(f"Balance: ${users[user]}")
+        await ctx.send(f"```Balance: ${users[user]}```")
     else:
-        await ctx.send("You are not registered")
+        await ctx.send("```You are not registered```")
 
 #Coin flip
 @bot.command()
@@ -81,33 +79,33 @@ async def coinflip(ctx, bet, side):
         with open("users.json") as file:
             users = json.load(file)
     except (json.JSONDecodeError):
-        await ctx.send("You are not registered")
+        await ctx.send("```You are not registered. Please type '>register'.```")
         return
     if user not in users.keys():
-        await ctx.send("You are not registered")
+        await ctx.send("```You are not registered. Please type '>register'.```")
         return
     
     #check if user has enough money
-    if users[user] < int(bet):
-        await ctx.send("You do not have enough money")
+    if float(users[user]) < float(bet):
+        await ctx.send("```You do not have enough money```")
         return
     
     #check if side is valid
     if side != "heads" and side != "tails":
-        await ctx.send("Invalid side")
+        await ctx.send("```Invalid side```")
         return
     
     #update balance
-    users[user] -= int(bet)
+    users[user] = float(users[user])-float(bet)
+    users[user] = format(users[user],".2f")
 
     #check if user won and update balance
     if side == outcome:
-        users[user] += 2*int(bet)
-        await ctx.send(f"You won!")
-        await ctx.send(f"New balance: ${users[user]}")
+        users[user] = float(users[user])+(2*float(bet))
+        users[user] = format(users[user],".2f")
+        await ctx.send(f"```You won!\nNew balance: ${users[user]}```")
     else:
-        await ctx.send(f"You lost!")
-        await ctx.send(f"New balance: ${users[user]}")
+        await ctx.send(f"```You lost!\nNew balance: ${users[user]}```")
 
 
     #save balance
@@ -127,29 +125,26 @@ async def highlow(ctx, bet):
         with open("users.json") as file:
             users = json.load(file)
     except (json.JSONDecodeError):
-        await ctx.send("You are not registered")
+        await ctx.send("```You are not registered. Please type '>register'.```")
         return
     if user not in users.keys():
-        await ctx.send("You are not registered")
+        await ctx.send("```You are not registered. Please type '>register'.```")
         return
     
     #check if user has enough money
-    if users[user] < int(bet):
-        await ctx.send("You do not have enough money")
+    if float(users[user]) < float(bet):
+        await ctx.send("```You do not have enough money```")
         return
     
     #update balance
-    users[user] -= int(bet)
+    users[user] = float(users[user])-float(bet)
+    users[user] = format(users[user],".2f")
 
     #begin game
-    await ctx.send("You will have 10 seconds to make your guess of higher or lower.")
-    time.sleep(1)
-    await ctx.send("Numbers are between 1 and 100.")
-    time.sleep(1)
-    await ctx.send("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    time.sleep(1)
+    await ctx.send("```You will have 10 seconds to make your guess of higher or lower.\nNumbers are between 1 and 100.```")
+    time.sleep(2)
     while isCorrect:
-        await ctx.send(f"Will the next number be higher or lower than {startNumber}?")
+        await ctx.send(f"```Will the next number be higher or lower than {startNumber}?```")
         def check(m):
             return m.author == ctx.author
         
@@ -163,11 +158,10 @@ async def highlow(ctx, bet):
                     await ctx.send("Invalid guess, please enter 'higher' or 'lower'.")
             newNumber = random.randint(1,100)
             if (newNumber > startNumber and guess.content.lower() == "higher") or (newNumber < startNumber and guess.content.lower() == "lower"):
-                await ctx.send(f"Correct! The number was {newNumber}")
+                
                 startNumber = newNumber
-                payout = int(payout)*2
-                await ctx.send(f"Current payout: ${payout}")
-                await ctx.send(f"Continue? (yes/no)")
+                payout = format(float(payout)*2,".2f")
+                await ctx.send(f"```Correct! The number was {newNumber}\nCurrent payout: ${payout}\nContinue? (yes/no)```")
                 try:
                     validResponse = False
                     while not validResponse:
@@ -175,28 +169,27 @@ async def highlow(ctx, bet):
                         if cont.content in ["yes","no"]:
                             validResponse = True
                         if not validResponse:
-                            await ctx.send("Invalid response please enter 'yes' or 'no'.")
+                            await ctx.send("```Invalid response please enter 'yes' or 'no'.```")
                     if cont.content.lower() == "no":
                         break
                     else:
                         continue
                 except:
-                    await ctx.send("You took too long to respond")
-                    await ctx.send(f"New balance: ${users[user]}")
+                    await ctx.send(f"```You took too long to respond\nNew balance: ${users[user]}```")
                     return
 
             else:
-                await ctx.send(f"Incorrect! Number was {newNumber}")
+                await ctx.send(f"```Incorrect! Number was {newNumber}.```")
                 payout = 0
                 isCorrect = False
         except:
-            await ctx.send("You took too long to respond")
-            await ctx.send(f"New balance: ${users[user]}")
+            await ctx.send(f"```You took too long to respond.\nNew balance: ${users[user]}```")
             return
     
     #update balance
-    users[user] += payout
-    await ctx.send(f"New balance: ${users[user]}")
+    users[user] = float(users[user])+float(payout)
+    users[user] = format(users[user],".2f")
+    await ctx.send(f"```New balance: ${users[user]}```")
 
     #save balance
     with open("users.json", "w") as file:
@@ -212,33 +205,31 @@ async def reset(ctx):
         with open("users.json") as file:
             users = json.load(file)
     except (json.JSONDecodeError):
-        await ctx.send("You are not registered")
+        await ctx.send("```You are not registered. Please type '>register'```")
         return
     if user not in users.keys():
-        await ctx.send("You are not registered")
+        await ctx.send("```You are not registered. Please type '>register'```")
         return
     
     #make sure
-    await ctx.send("Are you sure you want to reset your balance? (yes/no)")
+    await ctx.send("```Are you sure you want to reset your balance? (yes/no)```")
     def check(m):
         return m.author == ctx.author
     try:
         response = await bot.wait_for("message", check=check, timeout=10)
         if response.content not in ["yes", "no"]:
-            await ctx.send("Balance reset cancelled")
+            await ctx.send("```Balance reset cancelled```")
             return
         elif response.content.lower() != "yes":
-            await ctx.send("Balance reset cancelled")
+            await ctx.send("```Balance reset cancelled```")
             return
         else:
-            users[user] = 1000
+            users[user] = format(1000,".2f")
             with open("users.json", "w") as file:
                 json.dump(users, file)
-            await ctx.send("Balance reset")
-            await ctx.send("New balance: $1000")
+            await ctx.send("```Balance reset\nNew balance: $1000.00```")
     except:
-        await ctx.send("You took too long to respond")
-        await ctx.send("Balance reset cancelled")
+        await ctx.send("```You took too long to respond\nBalance reset Cancelled```")
         return
 
 #leaderboard
@@ -248,13 +239,14 @@ async def leaderboard(ctx):
         with open("users.json") as file:
             users = json.load(file)
     except (json.JSONDecodeError):
-        await ctx.send("No users registered")
+        await ctx.send("```No users registered```")
         return
     
     sorted_users = sorted(users.items())
     body = [list(pair) for pair in sorted_users]
     for i in range(len(body)):
         body[i].insert(0, i+1)
+        body[i][2] = f"${body[i][2]}"
     header = ["Rank", "User", "Balance"]
 
     table = t2a(header=["Rank","User","Balance"],
